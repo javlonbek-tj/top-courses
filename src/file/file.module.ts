b0b41f2e280542d08aa81de73911c526d748ed62
tjdeveloper <tjmr1996@gmail.com>
2023-07-27 17:02:31 +0500

    some changes

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import path from 'app-root-path';
+import { path } from 'app-root-path';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
 
--- src/top-page/top-page-model.ts
+++ src/top-page/top-page-model.ts
@@ -64,7 +64,7 @@ export class TopPage {
   @Prop({ required: true })
   tagsTitle: string;
 
-  @Prop([String])
+  @Prop({ type: () => [String] })
   tags: string[];
 }
 
